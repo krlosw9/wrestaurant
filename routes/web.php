8ffd2c5735cebc1fc0131b7be5de93fd3a075cee
Carlos Waldo Rojas <krlosw9@gmail.com>
2022-01-30 12:56:09 -0500

    Modificada la plantilla jetstream

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*Route::view('/', 'welcome')->name('home');*/
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+/*Route::get('/', function () {
+    return view('welcome');
+});*/
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/', function () {
     return view('dashboard');
-})->name('dashboard');
+})->name('home');
